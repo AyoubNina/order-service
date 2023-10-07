b1dfb7e4cda586530fc79cf30d6f37d8f30955a1
AyoubNina <jihene.ayoub@gmail.com>
2023-10-07 11:03:51 +0100

    last commit

---
+++ src/main/java/com/sesame/order/entity/Order.java
@@ -0,0 +1,24 @@
+package com.sesame.order.entity;
+
+
+import com.sesame.order.dto.FoodItemsDTO;
+import com.sesame.order.dto.Restaurant;
+import com.sesame.order.dto.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("order")
+public class Order {
+    private Integer orderId;
+    private List<FoodItemsDTO> foodItemsList;
+    private Restaurant restaurant;
+    private UserDTO userDTO;
+
+}
---
+++ src/main/java/com/sesame/order/mapper/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.sesame.order.mapper;
+
+
+import com.sesame.order.dto.OrderDTO;
+import com.sesame.order.entity.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface OrderMapper {
+
+    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
+
+    Order mapOrderDTOToOrder(OrderDTO orderDTO);
+    OrderDTO mapOrderToOrderDTO(Order order);
+
+}
